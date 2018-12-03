3686edf6c810f2606dee83aa14b31ea7a0f7e23d
Alex Yao <huangyu_yao@lolab-nthu.org>
2018-12-04 00:39:13 +0800

    sth wrong with connect_to_flysim

--- src/connect_to_flysim.cpp
+++ src/connect_to_flysim.cpp
@@ -61,12 +61,10 @@ int ReadFile(const string ConfFilename, const string ProFilename)
 	int FindFreq=0;
 	string tmp2line[2];
 	
-	while(!ReadPro.eof())
-	{	
+	while(!ReadPro.eof()){	
 		getline(ReadPro, tmp);
 		
-		if(tmp.find(ProFile.FirstEventTime)!=string::npos) 
-		{
+		if(tmp.find(ProFile.FirstEventTime)!=string::npos){
 			tmp2line[0]=tmp;//add 1st line
 			getline(ReadPro, tmp);
 			if(tmp.find("Type")!=string::npos)
@@ -80,16 +78,13 @@ int ReadFile(const string ConfFilename, const string ProFilename)
 				tmp2line[0]="";
 				tmp2line[1]="";
 			}
-			continue;
-		}
-		else if((tmp.find("EndEvent")!=string::npos)&&(FindFreq==1))
-		{
+			continue;//ChangeMemPot is been neglected?
+		}else if((tmp.find("EndEvent")!=string::npos)&&(FindFreq==1)){
 			FindFreq=0;
 			continue;
 		}
 		
-		if(FindFreq==1)
-		{
+		if(FindFreq==1){
 			
 			if(tmp.find("Population")!=string::npos)
 			{
@@ -102,9 +97,7 @@ int ReadFile(const string ConfFilename, const string ProFilename)
 				tmpTypeFreq.Population=atoi(tmpnum.c_str());
 				ProFile.TypeFreq.insert(ProFile.TypeFreq.end(), tmpTypeFreq);
 			}
-		}
-		else if(FindFreq==0 && tmp[0]>' ')
-		{
+		}else if(FindFreq==0 && tmp[0]>' '){
 			ProFile.TypeMem=ProFile.TypeMem+tmp2line[0]+tmp2line[1]+tmp+"\n";
 			tmp2line[0]="";
 			tmp2line[1]="";
