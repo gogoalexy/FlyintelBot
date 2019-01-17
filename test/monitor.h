ad78217145910daa481353a7e77e0a0f369e36fb
Alex Yao <cktfg18nthu@gapp.nthu.edu.tw>
2019-01-17 19:40:39 +0800

    almost done

--- test/monitor.h
+++ test/monitor.h
@@ -4,19 +4,6 @@
 #include "max7219.h"
 
 
-class LEDmatrix {
-public:
-LEDmatrix();
-bool init();
-void registerWrite(unsigned char, unsigned char);
-void setBrightness();
-void setMode();
-
-private:
 
 
-};
-
-#define MAX7219_DIGIT_REG(pos)  ((pos) + 1)
-#define MAX7219_COLUMN_REG(pos) MAX7219_DIGIT_REG(pos)
 
