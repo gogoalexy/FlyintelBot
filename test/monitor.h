6e2348a3e118c94117ad3fa302aff5c400adabf3
Alex Yao <huangyu_yao@lolab-nthu.org>
2019-01-24 15:04:10 +0800

    monitor utility

--- test/monitor.h
+++ test/monitor.h
@@ -12,8 +12,24 @@ public:
 	NeuroMonitor();
 	int init(int, int);
 	int sortinghat(); //friend of flyintel
+	void registerWrite(int, unsigned char, unsigned char);
 	int visualize(NetStruct);
 	void refresh();
+	
+	inline unsigned char CATHODE_FUNC(int position)
+    {
+        return 0x01 + position;
+    }
+
+    inline unsigned char ANODE_FUNC(int position)
+    {
+        return 0x80 >> position;
+    }
+
+    inline unsigned int GLUE(unsigned char addr1, unsigned char addr2)
+    {
+        return addr1 | addr2 << 8;
+    } 
 
 private:
 	int state;
