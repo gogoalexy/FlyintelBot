94168ed43f190b63056aa0668622d44b3d187eaf
Alex Yao <huangyu_yao@lolab-nthu.org>
2019-03-07 03:23:05 +0800

    unknown issue: network behavior

---
+++ src/SCXmodel.h
@@ -0,0 +1,47 @@
+#ifndef SCX_MODEL_H
+
+#include "connect_to_flysim.h"
+#include "flyintel.h"
+#include <array>
+#include <queue>
+#include <string>
+#include <iostream>
+#include <tuple>
+#include "max7219.h"
+
+class SimpleCXStimulator
+{
+public:
+    void stiLoc(int, int);
+    void shiftRight(int);
+    void shiftLeft(int);
+
+};
+
+class SimpleCXDecoder : public Flyintel
+{
+public:
+    std::array<int, 16> sortingHat(char*);
+    std::queue<int> findBump();
+    void clean();
+private:
+    std::array<int, 16> eb;
+
+};
+
+typedef std::tuple<int, int, BYTE> LEDpacket;
+
+class SimpleCXMonitor
+{
+public:
+    SimpleCXMonitor();
+    void init();
+    void flush();
+    void showBump(std::queue<int>);
+private:
+    max7219 chip;
+    std::array<LEDpacket, 16> EB2Monitor;
+};
+
+#define SCX_MODEL_H
+#endif
---
+++ src/main_scx.cpp
@@ -0,0 +1,129 @@
+#include <signal.h>
+#include <time.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <array>
+#include "connect_to_flysim.h"
+#include "SCXmodel.h"
+#include "max7219.h"
+
+using namespace std;
+
+static bool run_flag = true;
+void handle_SIGINT(int unused)
+{
+   //On CTRL+C - abort! //
+    run_flag = false;
+    CloseSim();
+}
+
+char *Spikes = nullptr;
+
+
+int main()
+{
+    bool holdTarget = false;
+    bool newTarget = false;
+    int pastNew = -1;
+    enum Actions{Stop, Forward, Backward, Left, Right};
+    Actions state = Stop;
+
+	SimpleCXStimulator CXsti;
+	SimpleCXDecoder CXdecode;
+	SimpleCXMonitor CXled;
+	CXled.init();
+
+    fstream fp;
+    fp.open("Flyintel.log", ios::out);
+
+    string conf_file = "./networks/network_scxhz.conf", pro_file = "./networks/network_scx.pro";
+    int ErrorNumFromReadFile = ReadFile(conf_file, pro_file);
+	cout<<"ErrorNumFromReadFile="<<ErrorNumFromReadFile<<endl<<endl;
+
+    signal(SIGINT, handle_SIGINT);
+
+    for(int round=0; round<1000; ++round)
+    {
+    	clock_t tik = clock();
+
+        if(round <=10)
+            state = Stop;
+        else if(round <= 11)
+        {
+            state = Stop;
+            newTarget = true;
+        }
+        else if(round <= 50)
+            state = Forward;
+        else if(round <= 70)
+            state = Backward;
+        else if(round <= 100)
+            state = Stop;
+        else
+            state = Right;
+
+       
+        if(pastNew >= 0)
+    	{
+    	    cout<<"pastnew"<<pastNew<<'\n';
+    	    CXsti.stiLoc(pastNew, 0);
+    	    pastNew = -1;
+    	    holdTarget = true;
+    	}
+    	else if(newTarget)
+    	{
+    	    //.......locate
+    	    cout<<"newTar"<<'\n';
+  	    CXsti.stiLoc(6, 180);
+            newTarget = false;
+            holdTarget = false;
+            pastNew = 6;
+    	}
+    	else if(holdTarget)
+    	{
+    	    if(state == Stop)
+    	    {
+    	        cout<<"stop"<<'\n';
+    	    }
+    	    else if(state == Forward)
+    	    {
+    	        cout<<"straight"<<'\n';
+    	    }
+    	    else if(state == Backward)
+    	    {
+    	        cout<<"back"<<'\n';
+    	    }
+        	else if(state == Left)
+    	    {
+    	        cout<<"left"<<'\n';
+    	        CXsti.shiftLeft(200);
+    	    }
+        	else if(state == Right)
+            {
+                cout<<"right"<<'\n';
+                CXsti.shiftRight(200);
+            }
+        }
+        else
+        {
+            cout<<"No sti"<<'\n';
+        }
+
+        Spikes = ActiveSimGetSpike("250");
+    	cout
+		<<"receving\n";
+		//cout<<"Spikes:"<<endl<<Spikes<<endl;
+        CXled.flush();
+        auto tmp = CXdecode.sortingHat(Spikes);
+        auto ans (CXdecode.findBump());
+        CXled.showBump(ans);
+        cout<<endl;
+        CXdecode.clean();
+
+        clock_t tok = clock();
+        cout<<"time:"<<(tok-tik)/(double)CLOCKS_PER_SEC<<endl;
+    }
+
+    return 0;
+}
