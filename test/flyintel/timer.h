e7e45cb1d891b5d000f5f41ab7dec9f599f6de9c
Alex Yao <huangyu_yao@lolab-nthu.org>
2019-03-12 01:14:13 +0800

    timer base class

--- test/flyintel/timer.h
+++ test/flyintel/timer.h
@@ -1,1 +1,19 @@
+#ifndef TIMER_H
+
 #include <chrono>
+
+class Timer
+{
+public:
+    Timer();
+    void toZero();
+    void start();
+    bool isStarted();
+    unsigned long getMillis();
+
+private:
+    std::chrono::system_clock::time_point m_start;
+}
+
+#define TIMER_H
+#endif
