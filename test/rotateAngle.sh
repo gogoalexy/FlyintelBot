cb517bde2340fce95295237e4cb504392d91f4c7
Alex Yao <huangyu_yao@lolab-nthu.org>
2019-03-13 09:18:20 +0800

    all tested except pixy2

--- test/rotateAngle.sh
+++ test/rotateAngle.sh
@@ -4,7 +4,7 @@ ENA=23
 ENB=24
 ENC=1
 END=26
-IN=(3 2 4 5 22 21 25 27)
+IN=(3 2 4 5 21 22 25 27)
 
 gpio mode ${ENA} pwm
 gpio mode ${ENB} pwm
@@ -19,22 +19,33 @@ gpio mode ${IN[5]} out
 gpio mode ${IN[6]} out
 gpio mode ${IN[7]} out
 
+sleep 3
+
 echo "right"
 
-gpio pwm ${ENA} 500
-gpio pwm ${ENB} 500
-gpio pwm ${ENC} 500
-gpio pwm ${END} 500
+gpio pwm ${ENA} 250
+gpio pwm ${ENB} 250
+gpio pwm ${ENC} 550
+gpio pwm ${END} 550
 
 gpio write ${IN[0]} 1
 gpio write ${IN[1]} 0
-gpio write ${IN[2]} 1
-gpio write ${IN[3]} 0
+gpio write ${IN[2]} 0
+gpio write ${IN[3]} 1
 gpio write ${IN[4]} 1
 gpio write ${IN[5]} 0
-gpio write ${IN[6]} 1
-gpio write ${IN[7]} 0
+gpio write ${IN[6]} 0
+gpio write ${IN[7]} 1
 
-gpio delay 300
+sleep 0.3
+
+gpio write ${IN[0]} 0
+gpio write ${IN[1]} 0
+gpio write ${IN[2]} 0
+gpio write ${IN[3]} 0
+gpio write ${IN[4]} 0
+gpio write ${IN[5]} 0
+gpio write ${IN[6]} 0
+gpio write ${IN[7]} 0
 
 echo "Done."
